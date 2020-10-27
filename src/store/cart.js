96a197cf0cef09ca0360d458809ca21db08d8d9b
Vitor Dino <me@vitordino.com>
2020-10-27 02:29:19 -0300

    :sparkles: add reset fn

--- src/store/cart.js
+++ src/store/cart.js
@@ -3,11 +3,17 @@ import create from 'zustand'
 const getLocalStorage = k => JSON.parse(window.localStorage.getItem(k))
 const setLocalStorage = (k, v) => window.localStorage.setItem(k, JSON.stringify(v))
 
+const initialState = {}
+
 const useCart = create(set => ({
-	cart: getLocalStorage('cart') || {},
+	cart: getLocalStorage('cart') || initialState,
 	setCart: cart => set(() => {
     setLocalStorage('cart', cart)
     return { cart }
+  }),
+  reset: () => set(() => {
+    setLocalStorage('cart', initialState)
+    return { cart: initialState }
   }),
 }))
 
