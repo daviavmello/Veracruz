c9a733fbc5c663f888373ce228f2e2d6332b4158
Davi Mello <davimello1994@gmail.com>
2020-11-04 16:01:38 -0500

    🖼️  editing responsive attributes in home

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { Image } from 'bumbag'
 
 const Hero = () => (
-<div>
-<Image src={`/img/promo.jpg`} fit='cover' alt='Hero Cover' height='47rem' width='100%' marginBottom='4rem' backgroundColor='black' />
-</div>
+	<div>
+		<Image src={`/img/promo.jpg`} fit='cover' alt='Hero Cover' height='47rem' width='100%' marginBottom='4rem' display={{ default: 'flex', 'max-tablet': 'none'}} />
+        <Image src={`/img/promo-mobile.jpg`} fit='cover' alt='Hero Cover' height='30rem' width='100%' marginBottom='4rem' display={{ default: 'none', 'max-tablet': 'flex'}} />
+	</div>
 )
 
-export default Hero;
+export default Hero
