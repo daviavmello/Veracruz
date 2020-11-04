aa6b0f7d6b58f0a3076c05d72c746e1cab0b4f38
Davi Mello <davimello1994@gmail.com>
2020-11-04 17:19:12 -0500

    🖼️  adding promotional banner to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { Card, Box, Image, Stack, Heading, Container, Columns, Text, Paragraph, 
 
 import { products } from 'content'
 
-const Product = ({title, images, price }) => {
+const Product = ({ title, images, price }) => {
 	return (
 		<Box textAlign='center' marginBottom='2rem'>
 			<Card variant='shadowed' as='article' cursor='pointer' marginBottom='0.5rem' overflow='hidden' padding='0'>
@@ -23,27 +23,78 @@ const Product = ({title, images, price }) => {
 	)
 }
 
+const NextProduct = () => {
+	return (
+				<Box textAlign='center' marginBottom='2rem'>
+					<Card
+						maxHeight='12rem'
+						variant='shadowed'
+						as='article'
+						cursor='pointer'
+						marginBottom='0.5rem'
+						overflow='hidden'
+						padding='0'
+					>
+						<div style={{ }}>
+							<Image src={`/img/next.jpg`} />
+						</div>
+					</Card>
+					<Heading use='h5' padding='1rem 0 0.25rem 0' marginBottom='0'>
+						Moletom
+					</Heading>
+					<Text>
+						Em breve&nbsp;
+						<span role='img' aria-label='emojis'>
+							👀
+						</span>
+					</Text>
+				</Box>
+	)
+}
+
 const About = () => (
 	<Box marginY='4rem' spacing='major-4'>
 		<Heading use='h1'>Sobre a Veracruz</Heading>
 		<Columns>
 			<Columns.Column spreadTablet={12} spreadWidescreen={4} spread={4}>
-				<Badge textTransform='uppercase' padding='0.25rem 0.5rem' marginY='2rem'>Quem nós somos?&nbsp;<span role='img' aria-label='emojis'> 👋</span></Badge>
+				<Badge textTransform='uppercase' padding='0.25rem 0.5rem' marginY='2rem'>
+					Quem nós somos?&nbsp;
+					<span role='img' aria-label='emojis'>
+						{' '}
+						👋
+					</span>
+				</Badge>
 				<Paragraph>
-					Somos uma organização que tem como objetivo levar o evangelho ao mundo de uma maneira nunca vista antes, mostrando assim quem Jesus verdadeiramente É!{' '}
+					Somos uma organização que tem como objetivo levar o evangelho ao mundo de uma maneira nunca vista antes,
+					mostrando assim quem Jesus verdadeiramente É!{' '}
 				</Paragraph>
 			</Columns.Column>
 			<Columns.Column spreadTablet={12} spreadWidescreen={4} spread={4}>
-			<Badge textTransform='uppercase' padding='0.25rem 0.5rem' marginY='2rem'>Por que vestuário?&nbsp;<span role='img' aria-label='emojis'>👕</span></Badge>
+				<Badge textTransform='uppercase' padding='0.25rem 0.5rem' marginY='2rem'>
+					Por que vestuário?&nbsp;
+					<span role='img' aria-label='emojis'>
+						👕
+					</span>
+				</Badge>
 				<Paragraph>
-					A roupa que você veste fala muito sobre você, então porque não usar uma blusa que traga boas notícias?<br /> <br />“Com isso todos saberão que vocês são meus discípulos, se vocês se amarem uns aos outros”.” ‭‭João‬ ‭13:35‬
+					A roupa que você veste fala muito sobre você, então porque não usar uma blusa que traga boas notícias?
+					<br /> <br />
+					“Com isso todos saberão que vocês são meus discípulos, se vocês se amarem uns aos outros”.” ‭‭João‬ ‭13:35‬
 					‭NVI‬‬
 				</Paragraph>
 			</Columns.Column>
 			<Columns.Column spreadTablet={12} spreadWidescreen={4} spread={4}>
-			<Badge textTransform='uppercase' padding='0.25rem 0.5rem' marginY='2rem'>Coleção Veritas&nbsp;<span role='img' aria-label='emojis'>🌟</span></Badge>
+				<Badge textTransform='uppercase' padding='0.25rem 0.5rem' marginY='2rem'>
+					Coleção Veritas&nbsp;
+					<span role='img' aria-label='emojis'>
+						🌟
+					</span>
+				</Badge>
 				<Paragraph>
-					Veritas vem do latim, que significa “verdade”, e foi daí que surgiu o nome de nossa primeira coleção, pois acreditamos em João 8:32 “E conhecereis a verdade, e a verdade os libertará”. O mundo precisa conhecer a verdade do evangelho, a verdade que temos um Pai de amor e somos chamados de filhos de Deus! E aí, topa pregar essa mensagem?
+					Veritas vem do latim, que significa “verdade”, e foi daí que surgiu o nome de nossa primeira coleção, pois
+					acreditamos em João 8:32 “E conhecereis a verdade, e a verdade os libertará”. O mundo precisa conhecer a
+					verdade do evangelho, a verdade que temos um Pai de amor e somos chamados de filhos de Deus! E aí, topa pregar
+					essa mensagem?
 				</Paragraph>
 			</Columns.Column>
 		</Columns>
@@ -57,13 +108,14 @@ const ProductList = () => {
 				<Heading use='h1'>Produtos</Heading>
 				<Columns>
 					{products.map(product => (
-						<Columns.Column key={product.id} spreadTablet={12} spreadWidescreen={4} spread={4}>
-							<Link to={{pathname: `/${product.id}`, query: { id: product.id }}}>
+						<Columns.Column key={product.id} spreadTablet={12} spreadWidescreen={6} spread={6}>
+							<Link to={{ pathname: `/${product.id}`, query: { id: product.id } }}>
 								<Product {...product} key={product.id} />
 							</Link>
 						</Columns.Column>
 					))}
 				</Columns>
+						<NextProduct />
 			</Stack>
 			<About />
 		</Container>
@@ -73,8 +125,8 @@ const ProductList = () => {
 const Home = () => {
 	return (
 		<Container isFluid padding='0'>
-		<Hero />
-		<ProductList />
+			<Hero />
+			<ProductList />
 		</Container>
 	)
 }
