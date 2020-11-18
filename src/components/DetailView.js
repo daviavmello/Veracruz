78339aee5139d2e932974c4be5a83daa379dfae6
Davi Mello <davimello1994@gmail.com>
2020-11-17 19:39:15 -0500

    ✨ updating stock in DetailView

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -50,6 +50,9 @@ const Product = ({
 		 else if (id === 'pfs' && color === 'BLACK' && gender === 'BOY') {
 			return sizes.slice(0, 2).map(value => ({ value, label: dictionary.sizes[value] || value }))
 		 }
+		 else if (id === 'verse' && color === 'BLACK' && gender === 'GIRL') {
+			return sizes.slice(0, 2).map(value => ({ value, label: dictionary.sizes[value] || value }))
+		 }
 		else {
 			return sizes.map(value => ({ value, label: dictionary.sizes[value] || value }))
 		}
